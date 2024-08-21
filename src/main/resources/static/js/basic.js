9c8e8fca4b11b55b3cd8d5130d88dbd05fbf5e2a
cheol <cheoloffice4854@gmail.com>
2024-08-21 14:22:31 +0900

    login

---
+++ src/main/resources/static/js/basic.js
@@ -0,0 +1,28 @@
+let host = 'http://' + window.location.host;
+let Cookies;
+
+$(document).ready(function () {
+    const auth = getToken();
+    if(auth === '') {
+        window.location.href = host + "/api/user/login-page";
+    } else {
+        $('#login-true').show();
+        $('#login-false').hide();
+    }
+})
+
+function logout() {
+    // 토큰 삭제
+    Cookies.remove('Authorization', { path: '/' });
+    window.location.href = host + "/api/user/login-page";
+}
+
+function getToken() {
+    let auth = Cookies.get('Authorization');
+
+    if(auth === undefined) {
+        return '';
+    }
+
+    return auth;
+}
